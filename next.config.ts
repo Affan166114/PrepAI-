2fa54bf8a84f1c45cafa150f8e845951033b2b8b
S Md Affan <newoffiacc@gmail.com>
2026-01-29 17:49:00 +0530

    eslint from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,4 @@
-import type { NextConfig } from "next";
-
 const nextConfig = {
-  /* config options here */
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "ik.imagekit.io",
-  //       port: "",
-  //     },
-  //   ],
-  // },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
