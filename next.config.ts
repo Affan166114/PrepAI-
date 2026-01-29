579aa837b3ca7d9d93ab93271180bf3429a67889
S Md Affan <newoffiacc@gmail.com>
2026-01-29 17:21:25 +0530

    ignore eslint and typescript errors

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,22 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
-  reactCompiler: true,
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "ik.imagekit.io",
+  //       port: "",
+  //     },
+  //   ],
+  // },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
